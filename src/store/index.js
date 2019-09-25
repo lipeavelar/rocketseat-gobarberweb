5ca298aa3915c7a5f4955efe9f8046b608d39f79
Felipe Avelar <lipeavelar@gmail.com>
2019-09-24 22:17:46 -0300

    First commit for rocketseat lessons
    Signed-off-by: Felipe Avelar <lipeavelar@gmail.com>

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+
+import createStore from './createStore';
+import persistReducers from './persistReducers';
+
+import rootReducers from './modules/rootReducer';
+import rootSaga from './modules/rootSagas';
+
+const middlewares = [];
+
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+middlewares.push(sagaMiddleware);
+
+const store = createStore(persistReducers(rootReducers), middlewares);
+const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
+
+export { store, persistor };
---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,33 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import api from '../../../services/api';
+import history from '../../../services/history';
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (!user.provider) {
+      toast.error('Usuário não é um provedor');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error('Usuário ou senha inválido.');
+    yield put(signFailure());
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
