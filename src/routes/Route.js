da78a1d5ec3ce04310d585ed961d142de6439b4b
Felipe Avelar <lipeavelar@gmail.com>
2019-10-01 19:50:47 -0300

    Finished app
    Signed-off-by: Felipe Avelar <lipeavelar@gmail.com>

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -20,7 +20,7 @@ export default function RouterWrapper({
   }
 
   if (signed && !isPrivate) {
-    return <Redirect to="/dashoard" />;
+    return <Redirect to="/dashboard" />;
   }
 
   const Layout = signed ? DefaultLayout : AuthLayout;
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -21,6 +21,8 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
@@ -30,4 +32,40 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  const { name, email, password } = payload;
+  try {
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, por favor verifique seus dados');
+
+    yield put(signFailure);
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
